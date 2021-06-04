7b04d02a7d19571fa0aec94d6aacef91ff02c91f
Frank D. Martinez M <mnesarco@gmail.com>
2021-06-04 14:00:39 -0500

    start

---
+++ README.md
@@ -0,0 +1,165 @@
+# C++17 syntactic sugar for ImGui with RAII guards
+
+This single header library adds a clean DSL (syntax sugar) to
+construct ImGui interfaces in c++17. It manages the stack with transparent
+RAII guards.
+
+## Requirements
+
+* c++17
+* [Dear ImGui](https://github.com/ocornut/imgui)
+
+## Install
+
+Just add `imgui_sugar.hpp` to your sources and include it when required.
+
+## Example usage
+
+```cpp
+#include <imgui.h>
+#include <imgui_sugar.hpp>
+
+// ...
+
+    static int left = 0, right = 0;
+    ImGui::SetNextWindowPos(ImVec2(30, 50), ImGuiCond_FirstUseEver);
+    set_StyleColor(ImGuiCol_WindowBg, ImVec4{0.88f, 0.88f, 0.88f, 1.0f});        
+    set_StyleColor(ImGuiCol_Text, 0xff000000);
+
+    with_Window("Test Window", nullptr, ImGuiWindowFlags_AlwaysAutoResize) {
+
+        ImGui::Text("Hello");
+        
+        with_Group {
+            ImGui::Text("Left %d", left);
+            if (ImGui::Button("Incr Left"))
+                ++left;
+        }
+        
+        ImGui::SameLine();
+        
+        with_Group {
+            set_StyleColor(ImGuiCol_Text, 0xffff0000);
+        
+            ImGui::Text("Right %d", right);
+        
+            if (ImGui::Button("Incr Right"))
+                ++right;
+        
+            with_Child("##scrolling", ImVec2(200, 80)) {
+
+                ImGui::Text("More text ...");
+                ImGui::Text("More text ...");
+                ImGui::Text("More text ...");
+                
+                with_StyleColor(ImGuiCol_Text, ImVec4{ 0, 0.5f, 0, 1.0f })
+                    ImGui::Text("More text ...");
+                
+                ImGui::Text("More text ...");
+                
+                with_StyleColor(ImGuiCol_Text, ImVec4{ 0.5f, 0.0f, 0, 1.0f }) {
+                    ImGui::Text("More text ...");
+                    ImGui::Text("More text ...");
+                }
+            }
+        }
+
+        ImGui::Text("Bye...");
+    }    
+
+
+// ...
+
+```
+
+![screenshot.png](screenshot.png)
+
+## Rational
+
+* Functions named `with_*` do start its own scope and automatically call End/Pop as required. (Yes the case where End/Pop call is unconditionall is respected).
+
+```cpp
+with_Window(...) { 
+    // ....
+}
+
+// If the scope contains a single statement, brackets are optional as usual.
+with_StyleColor(ImGuiCol_Text, ImVec4{ 0, 0.5f, 0, 1.0f })
+    ImGui::Text("More text ...");
+
+```
+
+* Functions named `set_*` do not start its own scope but its state is managed by the surrounding scope.
+
+```cpp
+{
+    set_StyleColor(...);
+    // ...
+}
+
+```
+
+
+## Scope definitions 
+
+Sugar function                |Begin Call               |End Call |
+| --- | --- | --- |
+|with_Window(...) { ... }             |ImGui::Begin,                   |ImGui::End |               
+|with_Child(...) { ... }              |ImGui::BeginChild,              |ImGui::EndChild |          
+|with_ChildFrame(...) { ... }         |ImGui::BeginChildFrame,         |ImGui::EndChildFrame |     
+|with_Combo(...) { ... }              |ImGui::BeginCombo,              |ImGui::EndCombo |          
+|with_ListBox(...) { ... }            |ImGui::BeginListBox,            |ImGui::EndListBox |        
+|with_MenuBar(...) { ... }            |ImGui::BeginMenuBar,            |ImGui::EndMenuBar |        
+|with_MainMenuBar(...) { ... }        |ImGui::BeginMainMenuBar,        |ImGui::EndMainMenuBar |    
+|with_Menu(...) { ... }               |ImGui::BeginMenu,               |ImGui::EndMenu |           
+|with_Popup(...) { ... }              |ImGui::BeginPopup,              |ImGui::EndPopup |          
+|with_PopupModal(...) { ... }         |ImGui::BeginPopupModal,         |ImGui::EndPopup |          
+|with_PopupContextItem(...) { ... }   |ImGui::BeginPopupContextItem,   |ImGui::EndPopup |          
+|with_PopupContextWindow(...) { ... } |ImGui::BeginPopupContextWindow, |ImGui::EndPopup |          
+|with_PopupContextVoid(...) { ... }   |ImGui::BeginPopupContextVoid,   |ImGui::EndPopup |          
+|with_Table(...) { ... }              |ImGui::BeginTable,              |ImGui::EndTable |          
+|with_TabBar(...) { ... }             |ImGui::BeginTabBar,             |ImGui::EndTabBar |         
+|with_TabItem(...) { ... }            |ImGui::BeginTabItem,            |ImGui::EndTabItem |        
+|with_DragDropSource(...) { ... }     |ImGui::BeginDragDropSource,     |ImGui::EndDragDropSource | 
+|with_DragDropTarget(...) { ... }     |ImGui::BeginDragDropTarget,     |ImGui::EndDragDropTarget | 
+|with_Group { ... }                   |ImGui::BeginGroup,              |ImGui::EndGroup |
+|with_Tooltip { ... }                 |ImGui::BeginTooltip,            |ImGui::EndTooltip |
+|with_Font(...) { ... }               |ImGui::PushFont,                |ImGui::PopFont |               
+|with_AllowKeyboardFocus(...) { ... } |ImGui::PushAllowKeyboardFocus,  |ImGui::PopAllowKeyboardFocus | 
+|with_ButtonRepeat(...) { ... }       |ImGui::PushButtonRepeat,        |ImGui::PopButtonRepeat |       
+|with_ItemWidth(...) { ... }          |ImGui::PushItemWidth,           |ImGui::PopItemWidth |          
+|with_TextWrapPos(...) { ... }        |ImGui::PushTextWrapPos,         |ImGui::PopTextWrapPos |        
+|with_ID(...) { ... }                 |ImGui::PushID,                  |ImGui::PopID |                 
+|with_ClipRect(...) { ... }           |ImGui::PushClipRect,            |ImGui::PopClipRect |           
+|with_TextureID(...) { ... }          |ImGui::PushTextureID,           |ImGui::PopTextureID |          
+|with_StyleColor(...) { ... }         |ImGui::PushStyleColor,          |ImGui::PopStyleColor |           
+|with_StyleVar(...) { ... }           |ImGui::PushStyleVar,            |ImGui::PopStyleVar |          
+
+
+
+## Parent scoped guards 
+
+Sugar function |Begin Call |End Call |
+| --- | --- | --- |
+|set_StyleColor(...) |ImGui::PushStyleColor, |ImGui::PopStyleColor |           
+|set_StyleVar(...)   |ImGui::PushStyleVar,   |ImGui::PopStyleVar |          
+
+
+## Abstraction cost
+
+* All guards store a function pointer to the end function.
+* Some guards store a boolean var with the returned value from begin.
+* No heap allocations are done.
+
+
+## Disclaimers
+
+* No guarantees.
+* This is very opinionated, if you don't like it, just do not use it.
+* This uses templates and macros.
+* This is **not a proposal**, just something that you can use if you want.
+
+
+## Contributions
+
+Discussions and PRs are welcome :)
---
+++ imgui_sugar.hpp
@@ -0,0 +1,185 @@
+#pragma once
+
+// The MIT License (MIT)
+//
+// Copyright (c) 2021 Frank D. Martinez M. <aka mnesarco>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <imgui.h>
+#include <type_traits>
+#include <utility>
+
+// clang-format off
+
+// ----------------------------------------------------------------------------
+// [SECTION] RAII guard implemetations
+// ----------------------------------------------------------------------------
+
+namespace sugar
+{
+
+    // RAII scope guard for ImGui Begin* functions returning bool.
+    template<bool _always_call_end>
+    struct guard_bool
+    {
+        using end_fn_ptr = void(*)();
+
+        guard_bool(const bool state, const end_fn_ptr end) noexcept 
+            : _state(state), _end(end) {}
+    
+        ~guard_bool() noexcept 
+        {
+            if constexpr (_always_call_end) { _end(); }
+            else { if (_state) _end(); }
+        }
+        
+        operator bool() const noexcept { return _state; }
+
+        private:
+            bool _state;
+            end_fn_ptr _end;
+    };
+
+    // RAII scope guard for ImGui Begin*/Push* functions returning void
+    template<typename EndArg = void, typename ...Args>
+    struct guard_void
+    {
+        static_assert(
+            std::is_same<EndArg, int>::value || std::is_same<EndArg, void>::value, 
+            "Only End*/Pop* functions accepting int or void are supported"
+        );
+
+        using begin_fn_t = void (*)(Args...);
+        using end_void_fn_t = void (*)();
+        using end_int_fn_t = void (*)(int);
+
+        guard_void(begin_fn_t begin, end_void_fn_t end, Args&&... args) noexcept 
+            : _end(reinterpret_cast<end_void_fn_t>(end)) { begin(std::forward<Args>(args)...); }
+        
+        guard_void(begin_fn_t begin, end_int_fn_t end, Args&&... args) noexcept 
+            : _end(reinterpret_cast<end_void_fn_t>(end)) { begin(std::forward<Args>(args)...); }
+        
+        ~guard_void() noexcept 
+        {
+            if constexpr (std::is_same<EndArg, void>::value) _end();
+            else reinterpret_cast<end_int_fn_t>(_end)(1);
+        }
+        
+        constexpr operator bool() const noexcept { return true; }
+
+        private:
+            end_void_fn_t _end;
+    };
+
+}
+
+// ----------------------------------------------------------------------------
+// [SECTION] Utility macros
+// ----------------------------------------------------------------------------
+
+// Concatenating symbols with __LINE__ requires two levels of indirection
+#define _SUGAR_CONCAT0(A, B) A ## B
+#define _SUGAR_CONCAT1(A, B) _SUGAR_CONCAT0(A, B)
+
+// ----------------------------------------------------------------------------
+// [SECTION] Generic macros to simplify repetitive declarations
+// ----------------------------------------------------------------------------
+// 
+// +----------------------+-------------------+-----------------+---------------------+
+// | BEGIN                | END               | ALWAYS          | __VA_ARGS__         |
+// +----------------------+-------------------+-----------------+---------------------+
+// | ImGui::Begin*/Push*  | ImGui::End*/Pop*  | Is call to END  | ImGui::Begin*/Push* |
+// | function name        | function name     | unconditional?  | function arguments  |
+// +----------------------+-------------------+-----------------+---------------------+
+
+#define _SUGAR_SCOPED_BOOL(BEGIN, END, ALWAYS, ...) \
+    if (const sugar::guard_bool<ALWAYS> _ui_scope_guard = {ImGui::BEGIN(__VA_ARGS__), &ImGui::END})
+
+#define _SUGAR_SCOPED_VOID_N(BEGIN, END, ...) \
+    if (const sugar::guard_void _ui_scope_guard = {&ImGui::BEGIN, &ImGui::END, __VA_ARGS__})
+
+#define _SUGAR_SCOPED_VOID_0(BEGIN, END) \
+    if (const sugar::guard_void _ui_scope_guard = {&ImGui::BEGIN, &ImGui::END})
+
+#define _SUGAR_PARENT_SCOPED_VOID_N(BEGIN, END, ...) \
+    const sugar::guard_void _SUGAR_CONCAT1(_ui_scope_, __LINE__) = {&ImGui::BEGIN, &ImGui::END, __VA_ARGS__}
+
+// ---------------------------------------------------------------------------
+// [SECTION] ImGui DSL
+// ----------------------------------------------------------------------------
+
+// Self scoped blocks
+
+#define with_Window(...)             _SUGAR_SCOPED_BOOL(Begin,                   End,               true,  __VA_ARGS__)
+#define with_Child(...)              _SUGAR_SCOPED_BOOL(BeginChild,              EndChild,          true,  __VA_ARGS__)
+#define with_ChildFrame(...)         _SUGAR_SCOPED_BOOL(BeginChildFrame,         EndChildFrame,     true,  __VA_ARGS__)
+#define with_Combo(...)              _SUGAR_SCOPED_BOOL(BeginCombo,              EndCombo,          false, __VA_ARGS__)
+#define with_ListBox(...)            _SUGAR_SCOPED_BOOL(BeginListBox,            EndListBox,        false, __VA_ARGS__)
+#define with_MenuBar(...)            _SUGAR_SCOPED_BOOL(BeginMenuBar,            EndMenuBar,        false, __VA_ARGS__)
+#define with_MainMenuBar(...)        _SUGAR_SCOPED_BOOL(BeginMainMenuBar,        EndMainMenuBar,    false, __VA_ARGS__)
+#define with_Menu(...)               _SUGAR_SCOPED_BOOL(BeginMenu,               EndMenu,           false, __VA_ARGS__)
+#define with_Popup(...)              _SUGAR_SCOPED_BOOL(BeginPopup,              EndPopup,          false, __VA_ARGS__)
+#define with_PopupModal(...)         _SUGAR_SCOPED_BOOL(BeginPopupModal,         EndPopup,          false, __VA_ARGS__)
+#define with_PopupContextItem(...)   _SUGAR_SCOPED_BOOL(BeginPopupContextItem,   EndPopup,          false, __VA_ARGS__)
+#define with_PopupContextWindow(...) _SUGAR_SCOPED_BOOL(BeginPopupContextWindow, EndPopup,          false, __VA_ARGS__)
+#define with_PopupContextVoid(...)   _SUGAR_SCOPED_BOOL(BeginPopupContextVoid,   EndPopup,          false, __VA_ARGS__)
+#define with_Table(...)              _SUGAR_SCOPED_BOOL(BeginTable,              EndTable,          false, __VA_ARGS__)
+#define with_TabBar(...)             _SUGAR_SCOPED_BOOL(BeginTabBar,             EndTabBar,         false, __VA_ARGS__)
+#define with_TabItem(...)            _SUGAR_SCOPED_BOOL(BeginTabItem,            EndTabItem,        false, __VA_ARGS__)
+#define with_DragDropSource(...)     _SUGAR_SCOPED_BOOL(BeginDragDropSource,     EndDragDropSource, false, __VA_ARGS__)
+#define with_DragDropTarget(...)     _SUGAR_SCOPED_BOOL(BeginDragDropTarget,     EndDragDropTarget, false, __VA_ARGS__)
+
+#define with_Group                   _SUGAR_SCOPED_VOID_0(BeginGroup,   EndGroup)
+#define with_Tooltip                 _SUGAR_SCOPED_VOID_0(BeginTooltip, EndTooltip)
+
+#define with_Font(...)               _SUGAR_SCOPED_VOID_N(PushFont,               PopFont,               __VA_ARGS__)
+#define with_AllowKeyboardFocus(...) _SUGAR_SCOPED_VOID_N(PushAllowKeyboardFocus, PopAllowKeyboardFocus, __VA_ARGS__)
+#define with_ButtonRepeat(...)       _SUGAR_SCOPED_VOID_N(PushButtonRepeat,       PopButtonRepeat,       __VA_ARGS__)
+#define with_ItemWidth(...)          _SUGAR_SCOPED_VOID_N(PushItemWidth,          PopItemWidth,          __VA_ARGS__)
+#define with_TextWrapPos(...)        _SUGAR_SCOPED_VOID_N(PushTextWrapPos,        PopTextWrapPos,        __VA_ARGS__)
+#define with_ID(...)                 _SUGAR_SCOPED_VOID_N(PushID,                 PopID,                 __VA_ARGS__)
+#define with_ClipRect(...)           _SUGAR_SCOPED_VOID_N(PushClipRect,           PopClipRect,           __VA_ARGS__)
+#define with_TextureID(...)          _SUGAR_SCOPED_VOID_N(PushTextureID,          PopTextureID,          __VA_ARGS__)
+
+// Non self scoped guards (managed by parent scope)
+
+#define set_Font(...)                _SUGAR_PARENT_SCOPED_VOID_N(PushFont,               PopFont,               __VA_ARGS__)
+#define set_AllowKeyboardFocus(...)  _SUGAR_PARENT_SCOPED_VOID_N(PushAllowKeyboardFocus, PopAllowKeyboardFocus, __VA_ARGS__)
+#define set_ButtonRepeat(...)        _SUGAR_PARENT_SCOPED_VOID_N(PushButtonRepeat,       PopButtonRepeat,       __VA_ARGS__)
+#define set_ItemWidth(...)           _SUGAR_PARENT_SCOPED_VOID_N(PushItemWidth,          PopItemWidth,          __VA_ARGS__)
+#define set_TextWrapPos(...)         _SUGAR_PARENT_SCOPED_VOID_N(PushTextWrapPos,        PopTextWrapPos,        __VA_ARGS__)
+#define set_ID(...)                  _SUGAR_PARENT_SCOPED_VOID_N(PushID,                 PopID,                 __VA_ARGS__)
+#define set_ClipRect(...)            _SUGAR_PARENT_SCOPED_VOID_N(PushClipRect,           PopClipRect,           __VA_ARGS__)
+#define set_TextureID(...)           _SUGAR_PARENT_SCOPED_VOID_N(PushTextureID,          PopTextureID,          __VA_ARGS__)
+
+// Special case (overloaded functions StyleColor and StyleVar)
+
+#define set_StyleColor(ITEM, ...)                                                                                   \
+    const sugar::guard_void<int, ImGuiCol,                                                                          \
+        std::conditional<std::is_integral<decltype(__VA_ARGS__)>::value, ImU32, const ImVec4&>::type>               \
+        _SUGAR_CONCAT1(_ui_scope_, __LINE__) = {&ImGui::PushStyleColor, &ImGui::PopStyleColor, ITEM, __VA_ARGS__}
+
+#define set_StyleVar(ITEM, ...)                                                                                     \
+    const sugar::guard_void<int, ImGuiStyleVar,                                                                     \
+        std::conditional<std::is_arithmetic<decltype(__VA_ARGS__)>::value, float, const ImVec2&>::type>             \
+        _SUGAR_CONCAT1(_ui_scope_, __LINE__) = {&ImGui::PushStyleVar, &ImGui::PopStyleVar, ITEM, __VA_ARGS__}
+
+#define with_StyleColor(...) if (set_StyleColor(__VA_ARGS__))
+#define with_StyleVar(...)   if (set_StyleVar(__VA_ARGS__))
+
+// clang-format on
